f69c14a561bca49a720e5f2658acab144fd19bc1
vijendra <vijkush14@gmail.com>
2015-04-26 18:21:16 -0400

    Here, there, everywhere

---
+++ gs/forms.py
@@ -0,0 +1,68 @@
+from django import forms
+
+GS_SUBJECTS = (
+	('ECO', 'Economics'),
+	('HIS', 'History'),
+	('GEO', 'Geography'),
+	('CUL', 'Culture and Arts'),
+	('ENV', 'Environment and Ecology'),
+	('SCI', 'Science and Technology'),
+	('CRR', 'Current Affairs'),
+)
+
+BTAX = (
+	('KNO', 'Knowledge'),
+	('COM', 'Comprehension'),
+	('APP', 'Application'),
+)
+
+QUES_TYPES = (
+	('1', 'Single Choice Correct'),
+	('2', 'Two Choices Correct'),
+	('3', 'Three Choices Correct'),
+	('4', 'More than Three Correct'),
+	('5', 'Matching'),
+)
+
+ANS_CHOICE = (
+    ('ch1', 'Choice A'),
+    ('ch2', 'Choice B'),
+    ('ch3', 'Choice C'),
+    ('ch4', 'Choice D'),
+)
+
+
+
+class QuestionForm(forms.Form):
+	
+	ques_text = forms.CharField(max_length=512, 
+			widget=forms.Textarea(attrs={'cols': 40, 'rows': 14, 'placeholder': 'Question goes here'}))
+
+	ques_ch1 = forms.CharField(max_length=128, 
+			widget=forms.Textarea(attrs={'cols': 40, 'rows': 2, 'placeholder':'Choice A'}))
+
+	ques_ch2 = forms.CharField(max_length=128, 
+			widget=forms.Textarea(attrs={'cols': 40, 'rows': 2, 'placeholder':'Choice B'}))
+
+	ques_ch3 = forms.CharField(max_length=128, 
+			widget=forms.Textarea(attrs={'cols': 40, 'rows': 2, 'placeholder':'Choice C'}))
+
+	ques_ch4 = forms.CharField(max_length=128, 
+			widget=forms.Textarea(attrs={'cols': 40, 'rows': 2, 'placeholder':'Choice D'}))
+
+
+	ques_sub = forms.ChoiceField(widget=forms.RadioSelect, choices=GS_SUBJECTS)
+
+	ques_type = forms.ChoiceField(widget=forms.RadioSelect, choices=QUES_TYPES)
+
+	ques_bloom = forms.ChoiceField(widget=forms.RadioSelect, choices=BTAX)
+
+	ques_tags = forms.CharField(max_length=128, 
+			widget=forms.Textarea(attrs={'rows': 4, 'placeholder':'Comma-separated tags'}))
+
+	ques_ans = forms.ChoiceField(widget=forms.RadioSelect, choices=ANS_CHOICE)
+
+	ques_sol = forms.CharField(widget=forms.Textarea(attrs={'cols': 40, 'rows': 14, 'placeholder':'Optional: solution'}))
+	ques_dscore = forms.IntegerField(max_length=1, 
+				widget=forms.TextInput(attrs={'placeholder':'1-10'}))
+
---
+++ gs/models.py
@@ -0,0 +1,68 @@
+
+from django.db import models
+from django.contrib.auth.models import User
+
+from taggit.managers import TaggableManager
+
+class Quiz(models.Model):
+
+	title = models.CharField(max_length=32, blank=False)
+	doc = models.DateField(auto_now_add=True)
+	creator = models.OneToOneField(User)
+	
+
+	def __unicode__(self):
+		return u'%s' % (self.title)
+
+class Question(models.Model):	
+
+	text = models.TextField(max_length=512, blank=False)
+	author = models.ForeignKey('Author')
+	doc = models.DateField(auto_now_add=True)
+	dom = models.DateField(auto_now=True)
+	dscore = models.IntegerField()
+	btax = models.CharField(max_length=3)
+	subj = models.CharField(max_length=3)
+	qtype = models.CharField(max_length=1)
+	flag = models.CharField(max_length=16)
+	tags = TaggableManager()
+	quiz = models.ForeignKey('Quiz')
+
+	def __unicode__(self):
+		return u'%s' % (self.text)
+
+class Choice(models.Model):
+	
+	text = models.CharField(max_length=64, blank=False)
+	ques = models.ForeignKey('Question')
+	tags = TaggableManager()
+
+	def __unicode__(self):
+		return u'%s' % (self.text)
+
+class Answer(models.Model):
+
+	text = models.TextField(max_length=512)
+	ques = models.ForeignKey('Question')
+	choice = models.ForeignKey('Choice')
+	tags = models.CharField(max_length=32)
+
+	def __unicode__(self):
+		return u'%s' % (self.text)
+
+class Author(models.Model):
+
+	user = models.OneToOneField(User)
+	doj = models.DateField(auto_now_add=True)
+	subj = models.CharField(max_length=16)
+
+	def __unicode__(self):
+		return u'%s' % (self.user.username)	
+
+class Aspirant(models.Model):
+	
+	user = models.OneToOneField(User)
+	doj = models.DateField(auto_now_add=True)
+	
+	def __unicode__(self):
+		return u'%s' % (self.user.username)
